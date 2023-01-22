c32f9df4e1f9baec591f3745041803907395fb20
abcd567a <abcd567@hotmail.com>
2023-01-22 18:30:50 -0500

    Update install-dump1090-fa-on-Fedora.sh

--- install-dump1090-fa-on-Fedora.sh
+++ install-dump1090-fa-on-Fedora.sh
@@ -3,7 +3,7 @@
 INSTALL_FOLDER=/usr/share/dump1090-assets
 sudo mkdir -p ${INSTALL_FOLDER}
 
-echo -e "\e[01;32mInstalling Tools & Dependencies.... \e[39m"
+echo -e "\e[01;32mInstalling Tools & Dependencies.... \e[0;39m"
 sudo dnf install -y git
 sudo dnf install -y wget
 sudo dnf install -y make
@@ -13,7 +13,7 @@ sudo dnf install -y libusbx-devel
 sudo dnf install -y ncurses-devel
 sudo dnf install -y lighttpd
 
-echo -e "\e[01;32mBuild & Install librtlsdr from source code. \e[39m"
+echo -e "\e[01;32mBuild & Install librtlsdr from source code. \e[0;39m"
 cd ${INSTALL_FOLDER}
 git clone https://github.com/steve-m/librtlsdr.git
 cd ${INSTALL_FOLDER}/librtlsdr
@@ -23,13 +23,13 @@ sudo make
 sudo make install
 sudo ldconfig
 
-echo -e "\e[01;32mBuilding dump1090-fa linux binary from source code \e[39m"
+echo -e "\e[01;32mBuilding dump1090-fa linux binary from source code \e[0;39m"
 cd ${INSTALL_FOLDER}
 sudo git clone https://github.com/flightaware/dump1090.git dump1090-fa
 cd ${INSTALL_FOLDER}/dump1090-fa
 sudo make BLADERF=no DUMP1090_VERSION=$(git describe --tags | sed 's/-.*//')
 
-echo -e "\e[01;32mCopying necessary files from cloned source code to the computer...\e[39m"
+echo -e "\e[01;32mCopying necessary files from cloned source code to the computer...\e[0;39m"
 sudo cp ${INSTALL_FOLDER}/dump1090-fa/dump1090 /usr/bin/dump1090-fa
 sudo cp ${INSTALL_FOLDER}/dump1090-fa/debian/dump1090-fa.default /etc/default/dump1090-fa
 sudo cp ${INSTALL_FOLDER}/dump1090-fa/debian/dump1090-fa.service /usr/lib/systemd/system/dump1090-fa.service
@@ -38,16 +38,16 @@ sudo cp ${INSTALL_FOLDER}/dump1090-fa/debian/start-dump1090-fa /usr/share/dump10
 sudo mkdir -p /usr/share/skyaware/
 sudo cp -r ${INSTALL_FOLDER}/dump1090-fa/public_html /usr/share/skyaware/html
 
-echo -e "\e[01;32mAdding system user dump1090 and adding it to group rtlsdr... \e[39m"
-echo -e "\e[01;32mThe user dump1090 will run the dump1090-fa service \e[39m"
+echo -e "\e[01;32mAdding system user dump1090 and adding it to group rtlsdr... \e[0;39m"
+echo -e "\e[01;32mThe user dump1090 will run the dump1090-fa service \e[0;39m"
 sudo useradd --system dump1090 
-echo -e "\e[01;32mInstalling rtl-sdr to create group rtlsdr and adding the\e[39m"
-echo -e "\e[01;32muser dump1090 to group rtlsdr to enable it to use rtlsdr Dongle ... \e[39m"
+echo -e "\e[01;32mInstalling rtl-sdr to create group rtlsdr and adding the\e[0;39m"
+echo -e "\e[01;32muser dump1090 to group rtlsdr to enable it to use rtlsdr Dongle ... \e[0;39m"
 sudo dnf install rtl-sdr -y
 sudo usermod -a -G rtlsdr dump1090
 sudo systemctl enable dump1090-fa
 
-echo -e "\e[01;32mPerforming Lighttpd integration to display Skyaware Map ... \e[39m"
+echo -e "\e[01;32mPerforming Lighttpd integration to display Skyaware Map ... \e[0;39m"
 sudo cp ${INSTALL_FOLDER}/dump1090-fa/debian/lighttpd/89-skyaware.conf /etc/lighttpd/conf.d/89-skyaware.conf
 sudo cp ${INSTALL_FOLDER}/dump1090-fa/debian/lighttpd/88-dump1090-fa-statcache.conf /etc/lighttpd/conf.d/88-dump1090-fa-statcache.conf
 sudo chmod 666 /etc/lighttpd/lighttpd.conf
@@ -58,16 +58,20 @@ sudo chmod 644 /etc/lighttpd/lighttpd.conf
 sudo systemctl enable lighttpd
 sudo systemctl start lighttpd
 
-echo -e "\e[01;32mConfiguring SELinux to run permissive for httpd \e[39m"
-echo "This will enable lighttpd to pull aircraft data from folder /var/run/dump1090-fa/ :"
+echo -e "\e[01;32mConfiguring SELinux to run permissive for httpd \e[0;39m"
+echo -e "\e[01;32mThis will enable lighttpd to pull aircraft data \e[0;39m"
+echo -e "\e[01;32mfrom folder /var/run/dump1090-fa/ \e[0;39m"
 sudo semanage permissive -a httpd_t
 
-echo -e "\e[01;32mConfiguring Firewall to permit display of SkyView from LAN/internet \e[39m"
+echo -e "\e[01;32mConfiguring Firewall to permit display of SkyView from LAN/internet \e[0;39m"
 sudo firewall-cmd --add-service=http
 sudo firewall-cmd --runtime-to-permanent
 sudo firewall-cmd --reload
 echo -e "\e[01;31m(4) REBOOT Computer ... REBOOT Computer ... REBOOT Computer \e[39m"
 echo " "
-echo -e "\e[01;32m(5) See the Web Interface (Map etc) at\e[39m"
+echo -e "\e[01;32m(5) See the Web Interface (Map etc) at\e[0;39m"
 echo -e "\e[39m        $(ip route | grep -m1 -o -P 'src \K[0-9,.]*')/skyaware/ \e[39m" "\e[35m(IP-of-Computer/skyaware/) \e[39m"
+echo -e "\e[01;32m     OR \e[0;39m"
+echo -e "\e[39m        $(ip route | grep -m1 -o -P 'src \K[0-9,.]*'):8080 \e[39m" "\e[35m(IP-of-Computer:8080) \e[39m"
+echo " "
 

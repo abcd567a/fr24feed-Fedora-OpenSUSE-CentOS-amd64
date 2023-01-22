7ee43a9f139befe9440b6da329a6a301ae84a025
abcd567a <abcd567@hotmail.com>
2023-01-22 18:39:55 -0500

    Update install-dump1090-fa-on-Fedora.sh

--- install-dump1090-fa-on-Fedora.sh
+++ install-dump1090-fa-on-Fedora.sh
@@ -67,11 +67,12 @@ echo -e "\e[01;32mConfiguring Firewall to permit display of SkyView from LAN/int
 sudo firewall-cmd --add-service=http
 sudo firewall-cmd --runtime-to-permanent
 sudo firewall-cmd --reload
+echo " "
 echo -e "\e[01;31m(4) REBOOT Computer ... REBOOT Computer ... REBOOT Computer \e[39m"
 echo " "
 echo -e "\e[01;32m(5) See the Web Interface (Map etc) at\e[0;39m"
 echo -e "\e[39m        $(ip route | grep -m1 -o -P 'src \K[0-9,.]*')/skyaware/ \e[39m" "\e[35m(IP-of-Computer/skyaware/) \e[39m"
-echo -e "\e[01;32m     OR \e[0;39m"
+echo -e "\e[01;32m      OR \e[0;39m"
 echo -e "\e[39m        $(ip route | grep -m1 -o -P 'src \K[0-9,.]*'):8080 \e[39m" "\e[35m(IP-of-Computer:8080) \e[39m"
 echo " "
 

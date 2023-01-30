dfbb38fabc884284b7d25d7e51c066f3792e2db6
abcd567a <abcd567@hotmail.com>
2023-01-30 02:54:17 -0500

    Update CentOS-8-9-dump1090-mutability.sh

--- CentOS-8-9-dump1090-mutability.sh
+++ CentOS-8-9-dump1090-mutability.sh
@@ -24,12 +24,12 @@ sudo yum install -y lighttpd
 
 echo -e "\e[01;32mDownloading dump1090-mutability Source Code from Github \e[0;39m"
 cd ${ASSETS_FOLDER}
-sudo git clone -b unmaintained https://github.com/mutability/dump1090.git
-cd ${ASSETS_FOLDER}/dump1090
+sudo git clone -b unmaintained https://github.com/abcd567a/dump1090-mutability dump1090-mutab
+cd ${ASSETS_FOLDER}/dump1090-mutab
 sudo make
 ##sudo make DUMP1090_VERSION=$(git describe --tags | sed 's/-.*//')
 echo -e "\e[01;32mCopying Executeable Binary to folder `/usr/bin/` \e[0;39m"
-sudo cp ${ASSETS_FOLDER}/dump1090/dump1090 /usr/bin/dump1090-mutability
+sudo cp ${ASSETS_FOLDER}/dump1090-mutab/dump1090 /usr/bin/dump1090-mutability
 
 sudo wget -O ${ASSETS_FOLDER}/dump1090-mutability.default  https://github.com/abcd567a/fr24feed-Fedora-OpenSUSE-CentOS-amd64/raw/main/mutab/dump1090-mutability.default
 sudo wget -O ${ASSETS_FOLDER}/start-dump1090-mutability "https://github.com/abcd567a/fr24feed-Fedora-OpenSUSE-CentOS-amd64/raw/main/mutab/start-dump1090-mutability"
@@ -39,7 +39,7 @@ sudo wget -O ${ASSETS_FOLDER}/dump1090-mutability.service "https://github.com/ab
 
 echo -e "\e[01;32mCopying necessary downloaded files to the computer...\e[0;39m"
 sudo mkdir -p /usr/share/dump1090-mutability
-sudo cp -r ${ASSETS_FOLDER}/dump1090/public_html /usr/share/dump1090-mutability/html
+sudo cp -r ${ASSETS_FOLDER}/dump1090-mutab/public_html /usr/share/dump1090-mutability/html
 sudo chmod +x ${ASSETS_FOLDER}/start-dump1090-mutability
 sudo cp ${ASSETS_FOLDER}/start-dump1090-mutability /usr/share/dump1090-mutability/
 
@@ -51,9 +51,9 @@ sudo mkdir -p /etc/lighttpd/conf.d
 sudo cp ${ASSETS_FOLDER}/89-dump1090-mutability.conf  /etc/lighttpd/conf.d/
 
 echo -e "\e[01;32mAdding system user dump1090 and adding it to group rtlsdr... \e[0;39m"
-echo -e "\e[01;32mThe user dump1090 will run the dump1090-fa service \e[0;39m"
+echo -e "\e[01;32mThe user dump1090 will run the dump1090-mutability service \e[0;39m"
 sudo useradd --system dump1090
-echo -e "\e[01;32mInstalling rtl-sdr to create group rtlsdr and adding the\e[0;39m"
+echo -e "\e[01;32mHave installed rtl-sdr to create group rtlsdr, and now adding the\e[0;39m"
 echo -e "\e[01;32muser dump1090 to group rtlsdr to enable it to use rtlsdr Dongle ... \e[0;39m"
 sudo usermod -a -G rtlsdr dump1090
 

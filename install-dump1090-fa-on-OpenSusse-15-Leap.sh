97b76210053260cf5fbd9acda042597ebcd1ec63
abcd567a <abcd567@hotmail.com>
2025-01-14 02:33:43 -0500

    Update and rename OpenSUSE-15-dump1090-fa.sh to install-dump1090-fa-on-OpenSusse-15-Leap.sh

---
+++ install-dump1090-fa-on-OpenSusse-15-Leap.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+ASSETS_FOLDER=/usr/share/dump1090-assets
+mkdir -p ${ASSETS_FOLDER}
+
+echo -e "\e[01;32mInstalling Tools & Dependencies.... \e[0;39m"
+zypper refresh
+
+zypper install -y git
+zypper install -y make
+zypper install -y gcc
+zypper install -y binutils
+zypper install -y glibc
+zypper install -y fakeroot
+zypper install -y pkgconf
+zypper install -y ncurses-devel
+zypper install -y ncurses
+zypper install -y lighttpd
+
+zypper install -y libusb-1_0-0
+zypper install -y libusb-compat-devel
+zypper install -y rtl-sdr-devel
+
+echo -e "\e[01;32mDownloading file rtl-sdr.rules from Github \e[0;39m"
+wget -O ${ASSETS_FOLDER}/rtl-sdr.rules https://github.com/abcd567a/fr24feed-Fedora-OpenSUSE-CentOS-amd64/raw/main/rtl-sdr.rules
+echo -e "\e[01;32mBuilding dump1090-fa linux binary from source code \e[0;39m"
+cd ${ASSETS_FOLDER}
+git clone https://github.com/flightaware/dump1090.git dump1090-fa
+cd ${ASSETS_FOLDER}/dump1090-fa
+make RTLSDR=yes DUMP1090_VERSION=$(git describe --tags | sed 's/-.*//')
+
+echo -e "\e[01;32mCopying files from assets folder to appropriate folders...\e[0;39m"
+cp ${ASSETS_FOLDER}/rtl-sdr.rules /etc/udev/rules.d/rtl-sdr.rules
+cp ${ASSETS_FOLDER}/dump1090-fa/dump1090 /usr/bin/dump1090-fa
+mkdir -p /etc/default
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/dump1090-fa.default /etc/default/dump1090-fa
+
+mkdir -p /usr/share/dump1090-fa/
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/start-dump1090-fa /usr/share/dump1090-fa/start-dump1090-fa
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/generate-wisdom /usr/share/dump1090-fa/
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/upgrade-config /usr/share/dump1090-fa/
+mkdir -p /usr/lib/dump1090-fa
+cp ${ASSETS_FOLDER}/dump1090-fa/starch-benchmark  /usr/lib/dump1090-fa/
+mkdir -p /usr/share/skyaware/
+cp -r ${ASSETS_FOLDER}/dump1090-fa/public_html /usr/share/skyaware/html
+mkdir -p /usr/lib/systemd/system
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/dump1090-fa.service /usr/lib/systemd/system/dump1090-fa.service
+
+echo -e "\e[01;32mAdding system user dump1090 and adding it to group rtlsdr... \e[0;39m"
+echo -e "\e[01;32mThe user dump1090 will run the dump1090-fa service \e[0;39m"
+useradd --system dump1090 
+echo -e "\e[01;32mInstalling rtl-sdr to create group rtlsdr and adding the\e[0;39m"
+echo -e "\e[01;32muser dump1090 to group rtlsdr to enable it to use rtlsdr Dongle ... \e[0;39m"
+usermod -a -G rtlsdr dump1090
+systemctl enable dump1090-fa
+
+echo -e "\e[01;32mPerforming Lighttpd integration to display Skyaware Map ... \e[0;39m"
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/lighttpd/89-skyaware.conf /etc/lighttpd/conf.d/89-skyaware.conf
+cp ${ASSETS_FOLDER}/dump1090-fa/debian/lighttpd/88-dump1090-fa-statcache.conf /etc/lighttpd/conf.d/88-dump1090-fa-statcache.conf
+chmod 666 /etc/lighttpd/lighttpd.conf
+echo "server.modules += ( \"mod_alias\" )" >> /etc/lighttpd/lighttpd.conf
+echo "include \"/etc/lighttpd/conf.d/89-skyaware.conf\"" >> /etc/lighttpd/lighttpd.conf
+sed -i 's/server.use-ipv6 = "enable"/server.use-ipv6 = "disable"/' /etc/lighttpd/lighttpd.conf
+chmod 644 /etc/lighttpd/lighttpd.conf
+systemctl enable lighttpd
+systemctl start lighttpd
+
+echo " "
+echo -e "\e[01;32mConfiguring Firewall to permit display of SkyView from LAN/internet \e[0;39m"
+echo -e "\e[39m   sudo firewall-cmd --zone=public --add-service=http --permanent \e[39m"
+echo -e "\e[39m   sudo firewall-cmd --zone=public --add-service=https --permanent \e[39m"
+echo -e "\e[39m   sudo firewall-cmd --reload \e[39m"
+
+firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --zone=public --add-service=https --permanent
+firewall-cmd --reload
+
+echo " "
+echo -e "\e[01;32mInstallation of dump1090-fa completed....\e[0;39m"
+echo -e "\e[01;32mSee the Web Interface (Map etc) at\e[0;39m"
+echo -e "\e[39m     $(ip route | grep -m1 -o -P 'src \K[0-9,.]*')/skyaware/ \e[39m" "\e[35m(IP-of-Computer/skyaware/) \e[39m"
+echo -e "\e[01;32m   OR \e[0;39m"
+echo -e "\e[39m     $(ip route | grep -m1 -o -P 'src \K[0-9,.]*'):8080 \e[39m" "\e[35m(IP-of-Computer:8080) \e[39m"
+echo " "
+echo -e "\e[01;32mControl dump1090-fa by following commands \e[0;39m"
+echo -e "\e[01;39m   sudo systemctl status dump1090-fa\e[0;39m"
+echo -e "\e[01;39m   sudo systemctl restart dump1090-fa\e[0;39m"
+echo " "
+echo -e "\e[01;31mREBOOT Computer ... REBOOT Computer ... REBOOT Computer \e[0;39m"
+echo " "
+

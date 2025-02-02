e3bd40786def1311b7e39989b4d2ffd95e8bbc25
abcd567a <abcd567@hotmail.com>
2025-02-02 13:32:37 -0500

    Update install-FR24-amd64.sh

--- install-FR24-amd64.sh
+++ install-FR24-amd64.sh
@@ -47,7 +47,6 @@ RuntimeDirectoryMode=0755
 ExecStartPre=-/bin/mkdir -p /run/fr24feed
 ExecStartPre=-/bin/touch /dev/shm/decoder.txt
 ExecStartPre=-/bin/chown fr24 /dev/shm/decoder.txt /run/fr24feed
-##ExecStartPre=/usr/bin/fr24feed --validate-config --config-file=/etc/fr24feed.ini
 ExecStart=/usr/bin/fr24feed
 ExecStop=/bin/kill -TERM $MAINPID
 User=fr24
